069c936e244c5f9c40f7e016021ff318d95c891d
Alexander30072004 <baceli03@tecnologicosudamericano.edu.ec>
2024-06-05 11:36:44 -0500

    Add files via upload

---
+++ serie de fibonaci/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    #levantar serie de fibonacci
+    print("SERIE FIBONACCI")
+    n=int(input("Cuantos digitos de la serie desea "))
+    a=1
+    b=1
+            for i in range(0,n):
+    print(a,end=" ")
+    c=a+b
+            a=b
+    b=c
+}
